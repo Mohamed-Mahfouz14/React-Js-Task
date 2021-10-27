cd62cbd77ceca8fa1a95a0ab7ff51f5e0a60c14f
mohamed-mahfouz14 <mohamed.mahfouz944@gmail.com>
2021-10-27 17:57:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Component } from "react";
+import "./App.css";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { FacebookLoginButton } from "react-social-login-buttons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Form className="login-form">
+        <h1>
+          <span className="font-weight-bold">gomycode</span>.com
+        </h1>
+        <h2 className="text-center">Welcome</h2>
+        <FormGroup className="mt-3 mb-3">
+          <Label>Email</Label>
+          <Input type="email" placeholder="Email" />
+        </FormGroup>
+
+        <FormGroup className="mt-3 mb-3">
+          <Label>Password</Label>
+          <Input type="password" placeholder="Password" />
+        </FormGroup>
+        <div>
+          <Button color="secondary" style={{ display: "block" }}>
+            Log in
+          </Button>
+        </div>
+
+        <div className="text-center pt-3">
+          Or continue with your social account
+        </div>
+        <FacebookLoginButton />
+      </Form>
+    );
+  }
 }
 
 export default App;
